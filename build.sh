b2afc582ac05b0487fa6586c39a1c30148f86837
Divine <divineanayo045gmail.com>
2026-07-19 15:45:15 -0700

    force clear static files on build

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # collect staticfiles
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 # to make migrations and migrate
 python manage.py makemigrations
